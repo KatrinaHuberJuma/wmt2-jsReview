9ed20508baef25784a941b3bab133fc20735fd67
Katrina Huber-Juma <Katrina.huber@gmail.com>
2022-04-20 12:45:56 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,44 @@
+"""Server for JavaScript: Sharkwords."""
+
+from flask import Flask, render_template
+from random import choice
+
+app = Flask(__name__)
+
+# WORDS = [
+#   'strawberry',
+#   'orange',
+#   'apple',
+#   'banana',
+#   'pineapple',
+#   'kiwi',
+#   'peach',
+#   'pecan',
+#   'eggplant',
+#   'durian',
+#   'peanut',
+#   'chocolate',
+# ];
+
+# @app.route('/words.json')
+# def get_word():
+#     return {"word": choice(WORDS)}
+    
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+
+@app.route("/demo")
+def demo():
+    return render_template("demo.html")
+
+
+@app.route("/sharkwords")
+def sharkwords():
+    return render_template("sharkwords.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
